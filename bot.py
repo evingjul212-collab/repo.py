17e3f8f67e5bbad00f35a84db298150862646a0d
evingjul212-collab <evingjul212@gmail.com>
2026-04-24 22:02:41 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -205,7 +205,7 @@ async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await q.edit_message_text("📂 Pilih Slot untuk Muat:", reply_markup=InlineKeyboardMarkup(kb))
 
   # GANTI BAGIAN LOAD LU DENGAN INI (Fix Nama Tokoh Utama)
-   elif q.data.startswith("load_"):
+    elif q.data.startswith("load_"):
         from bson import ObjectId
         data = await archives.find_one({"_id": ObjectId(q.data.split("_")[1])})
         if data:
