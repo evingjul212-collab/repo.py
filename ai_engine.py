795d45996de555c5d702540a48f7f2b0498c184a
evingjul212-collab <evingjul212@gmail.com>
2026-05-08 20:41:28 +0800

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -1,43 +1,33 @@
-import asyncio
-from google import genai
-from config import GEMINI_API_KEY, MODELS
-
-client = genai.Client(api_key=GEMINI_API_KEY)
-
-
 async def generate(prompt):
 
     prompt = prompt[:12000]
 
-    for _ in range(3):
-        for model in MODELS:
-            try:
-                res = await asyncio.to_thread(
+    for model in MODELS:
+
+        try:
+            print(f"TRY MODEL: {model}")
+
+            response = await asyncio.wait_for(
+                asyncio.to_thread(
                     client.models.generate_content,
                     model=model,
                     contents=prompt
-                )
+                ),
+                timeout=25
+            )
 
-                if res and res.text:
-                    return res.text.strip(), model
+            if response and response.text:
+                return response.text.strip(), model
 
-           except Exception as e:
+        except asyncio.TimeoutError:
+            print(f"{model} TIMEOUT")
+            continue
 
-    err = str(e)
-
-    print(f"[{model}] ERROR:", err)
-
-    # skip model jika internal error
-    if "500" in err:
-        continue
-
-    if "429" in err:
-        await asyncio.sleep(3)
-        continue
-
-    continue
+        except Exception as e:
+            print(f"{model} ERROR:", e)
+            continue
 
     return (
-    "Server AI sedang sibuk, coba kirim lagi beberapa saat.",
-    "fallback"
-)
+        "AI sedang sibuk, coba lagi beberapa saat.",
+        "fallback"
+    )
