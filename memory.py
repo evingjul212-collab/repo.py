9b30f378323c293a7a9b451890d1d0e0a2786a8e
evingjul212-collab <evingjul212@gmail.com>
2026-05-10 10:37:17 +0800

    Update memory.py

--- memory.py
+++ memory.py
@@ -4,6 +4,10 @@
 import os
 import logging
 from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URL
+client = AsyncIOMotorClient(MONGO_URL)
+db = client.game_db
+users = db.user_states
 
 logger = logging.getLogger(__name__)
 
